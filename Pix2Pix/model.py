2b3ea4c6a65cedfbef47904908f2b7fa17ee605b
kangsanha <68986305+sanha9999@users.noreply.github.com>
2021-08-25 22:38:20 +0900

    add Discriminator(patch gan)

--- Pix2Pix/model.py
+++ Pix2Pix/model.py
@@ -94,6 +94,49 @@ class Generator(nn.modules):
 
         return u8
 
+# Discriminator
+
+class Dis_block(nn.modules):
+    def __init__(self, in_channels, out_channels, normalize=True):
+        super().__init__()
+
+        layers = [nn.Conv2d(in_channels, out_channels, 3, stride=2, padding= 1)]
+        if normalize:
+            layers.append(nn.InstanceNorm2d(out_channels))
+        layers.append(nn.LeakyReLU(0.2))
+
+        self.block = nn.Sequential(* layers)
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+""" 
+Discriminator는 patch gan을 사용하는데, patch gan은 조건부 gan으로 
+이미지를 분할하여 각 패치가 진짜인지 아닌지 판단한다.
+"""
+
+class Discriminator(nn.modules):
+    def __init__(self, in_channels = 3):
+        super().__init__()
+
+        self.stage_1 = Dis_block(in_channels*2, 64, normalize=False)
+        self.stage_2 = Dis_block(64,128)
+        self.stage_3 = Dis_block(128,256)
+        self.stage_4 = Dis_block(256,512)
+
+        self.patch = nn.Conv2d(512,1,3,padding=1) # 16x16 패치 생성
+
+    def forward(self,a,b):
+        x = torch.cat((a,b),1)
+        x = self.stage_1(x)
+        x = self.stage_2(x)
+        x = self.stage_3(x)
+        x = self.stage_4(x)
+        x = self.patch(x)
+        x = torch.sigmoid(x)
+        return x
+
 
 
 
