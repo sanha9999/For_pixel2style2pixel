e7597ae5d731781bde399429f550604d14f619fc
kangsanha <68986305+sanha9999@users.noreply.github.com>
2021-08-25 16:01:13 +0900

    add generator(UNet)

---
+++ Pix2Pix/model.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.loss import L1Loss
+
+# loss function
+loss_func = nn.BCELoss() # 이진분류
+loss_func_pix2pix = nn.L1Loss() # Pix2Pix에서는 L1 loss function가 추가된다.
+
+
+# generator , UNet을 model architecture로 사용한다.
+# UNet
+class UNetDown(nn.modules):
+    def __init__(self, in_channels, out_channels, normalize=True, dropout=0.0):
+        super().__init__()
+
+        layers = [nn.Conv2d(in_channels, out_channels, 4, stride=2, padding=1, bias=False)]
+
+        if normalize:
+            layers.append(nn.InstanceNorm2d(out_channels)) # Pix2Pix는 Bnorm대신 InstanceNorm을 사용하였다.
+
+        layers.append(nn.LeakyReLU(0.2))
+
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+        
+        self.down = nn.Sequential(* layers)
+
+    def forward(self, x):
+        x = self.down(x)
+        return x
+
+class UNetUp(nn.modules):
+    def __init__(self, in_channels, out_channels, dropout=0.0):
+        super().__init__()
+
+        layers = [
+            nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU()
+        ]
+
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+
+        self.up = nn.Sequential(* layers)
+    def forward(self, x, skip): # skip connection
+        x = self.up(x)
+        x = torch.cat((x, skip), 1)
+        return x
+
+class Generator(nn.modules):
+    def __init__(self, in_channels=3, out_channels=3):
+        super().__init__()
+
+        self.down1 = UNetDown(in_channels, 64, normalize=False)
+        self.down2 = UNetDown(64, 128)
+        self.down3 = UNetDown(128, 256)
+        self.down4 = UNetDown(256, 512, dropout=0.5)
+        self.down5 = UNetDown(512, 512, dropout=0.5)
+        self.down6 = UNetDown(512, 512, dropout=0.5)
+        self.down7 = UNetDown(512, 512, dropout=0.5)
+        self.down8 = UNetDown(512, 512,normalize=False, dropout=0.5)
+
+        self.up1 = UNetUp(512, 512, dropout=0.5)
+        self.up2 = UNetUp(1024, 512, dropout=0.5)
+        self.up3 = UNetUp(1024, 512, dropout=0.5)
+        self.up4 = UNetUp(1024, 512, dropout=0.5)
+        self.up5 = UNetUp(1024, 256)
+        self.up6 = UNetUp(512, 128)
+        self.up7 = UNetUp(256, 64)
+        self.up8 = nn.Sequential(
+            nn.ConvTranspose2d(128, 3, 4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        d2 = self.down2(d1)
+        d3 = self.down3(d2)
+        d4 = self.down4(d3)
+        d5 = self.down5(d4)
+        d6 = self.down6(d5)
+        d7 = self.down7(d6)
+        d8 = self.down8(d7)
+
+        u1 = self.up1(d8, d7)
+        u2 = self.up2(u1, d6)
+        u3 = self.up3(u2, d5)
+        u4 = self.up4(u3, d4)
+        u5 = self.up5(u4, d3)
+        u6 = self.up6(u5, d2)
+        u7 = self.up7(u6, d1)
+        u8 = self.up8(u7)
+
+        return u8
+
+
+
+
+
+
+
